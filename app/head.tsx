2e49eb88c16298bb8601ebd44310a9b012e984fa
G. Willson <greg@biomassiv.es>
2023-04-16 16:42:17 -0600

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Jordi Enric</title>
+      <title>Worldbridger One  -</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="UI Engineer" />
+      <meta name="description" content="Worldbridger One is a unified interndimentional workfoce for happiness & fulfillment optimized futures" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
